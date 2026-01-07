95da448c9725707c1c83b118a06c16473a27a834
PitocoRamo <pv33680@alunos.estgv.ipv.pt>
2026-01-07 22:30:59 +0000

    correção do sever.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import oficinaRoutes from "./routes/oficinaRoutes.js";
 import servicoRoutes from "./routes/servicoRoutes.js";
@@ -14,22 +15,17 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", authRoutes);
-app.use("/api/oficinas", oficinaRoutes);
-
 
 connectDB();
 
 app.use("/api/auth", authRoutes);
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor a correr na porta ${PORT}`);
-});
-
-/*
+app.use("/api/oficinas", oficinaRoutes);
 app.use("/api/servicos", servicoRoutes);
 app.use("/api/veiculos", veiculoRoutes);
 app.use("/api/turnos", turnoRoutes);
 app.use("/api/marcacoes", marcacaoRoutes);
-*/
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor a correr na porta ${PORT}`);
+});
